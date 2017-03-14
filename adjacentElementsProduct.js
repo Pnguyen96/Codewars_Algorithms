6c91e7181f9068963c1ec411c11cb444f8a4d816
Melvin Nguyen <pnguyen1995@gmail.com>
2017-03-14 01:06:49 -0500

    added changes

--- adjacentElementsProduct.js
+++ adjacentElementsProduct.js
@@ -1,1 +1,13 @@
-adjacentElementsProduct.js
+function adjacentElementsProduct(inputArray) {
+
+    var max = -1000;
+    for(var i = 0; i<= inputArray.length-1; i++)
+        {
+            var product = inputArray[i]*inputArray[i+1]
+            if(product > max)
+                {
+                    max = product;
+                }
+        }
+    return max;
+}
